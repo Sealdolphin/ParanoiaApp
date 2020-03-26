7dc448214c2c99df34c65998af31f637765e4b9d
Mihalovits Márk <mark.mihalovits@incquerylabs.com>
2020-03-26 03:04:05 +0100

    Updated security clearances

--- src/main/java/paranoia/core/SecurityClearance.java
+++ src/main/java/paranoia/core/SecurityClearance.java
@@ -3,21 +3,28 @@ package paranoia.core;
 import java.awt.Color;
 
 public enum SecurityClearance {
-    INFRARED(Color.BLACK),
-    RED(Color.RED),
-    ORANGE(Color.ORANGE),
-    YELLOW(Color.YELLOW),
-    GREEN(Color.GREEN),
-    BLUE(Color.BLUE),
-    INDIGO(new Color(56, 18, 91)),
-    VIOLET(new Color(175,0, 255)),
-    ULTRAVIOLET(Color.WHITE);
+    INFRARED(new Color(0,0,0), new Color(38, 38, 38), new Color(255, 255, 255)),
+    RED(new Color(217,0,0), new Color(191,48,48), new Color(255,255,255)),
+    ORANGE(new Color(217,108,0), new Color(191,120,48), new Color(0,0,0)),
+    YELLOW(new Color(217,217,0), new Color(191,191,48), new Color(0,0,0)),
+    GREEN(new Color(0,217,0), new Color(48,191,48), new Color(0,0,0)),
+    BLUE(new Color(0,36,217), new Color(48,72,191), new Color(255,255,255)),
+    INDIGO(new Color(126,0,217), new Color(131,48,191), new Color(255,255,255)),
+    VIOLET(new Color(217,0,217), new Color(191,48,191), new Color(255,255,255)),
+    ULTRAVIOLET(new Color(255,255,255), new Color(191,191,191), new Color(0,0,0));
 
     private Color color;
-    private SecurityClearance(Color color) {
+    private Color backgroundColor;
+    private Color fontColor;
+
+    SecurityClearance(Color color, Color bgColor, Color fontColor) {
         this.color = color;
+        this.backgroundColor = bgColor;
+        this.fontColor = fontColor;
     }
     public Color getColor() { return color; }
+    public Color getFontColor() {return fontColor; }
+    public Color getBackgroundColor() { return backgroundColor; }
     public int getShort() { return name().charAt(0); }
 
 }
