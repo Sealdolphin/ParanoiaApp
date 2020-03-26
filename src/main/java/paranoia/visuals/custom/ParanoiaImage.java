15bbae45c0304de36a069423c5f798a5a07bdc7c
Mihalovits Márk <mark.mihalovits@incquerylabs.com>
2020-03-26 03:44:29 +0100

    Images can have borders

--- src/main/java/paranoia/visuals/custom/ParanoiaImage.java
+++ src/main/java/paranoia/visuals/custom/ParanoiaImage.java
@@ -1,6 +1,8 @@
 package paranoia.visuals.custom;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
@@ -9,7 +11,12 @@ public class ParanoiaImage extends JPanel {
     private BufferedImage image;
 
     public ParanoiaImage(BufferedImage image){
+        this(image, false);
+    }
+
+    public ParanoiaImage(BufferedImage image, Boolean hasBorder){
         this.image = image;
+        if(hasBorder) setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
     }
 
     @Override
