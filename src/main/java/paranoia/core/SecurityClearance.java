132cd389fd6c4860cf6b3abf19e79a461dbe438d
Mihalovits Márk <mark.mihalovits@incquerylabs.com>
2020-03-27 20:13:42 +0100

    Updated colors of security clearances

--- src/main/java/paranoia/core/SecurityClearance.java
+++ src/main/java/paranoia/core/SecurityClearance.java
@@ -3,14 +3,14 @@ package paranoia.core;
 import java.awt.Color;
 
 public enum SecurityClearance {
-    INFRARED(new Color(0,0,0), new Color(38, 38, 38), new Color(255, 255, 255)),
-    RED(new Color(217,0,0), new Color(191,48,48), new Color(255,255,255)),
-    ORANGE(new Color(217,108,0), new Color(191,120,48), new Color(0,0,0)),
-    YELLOW(new Color(217,217,0), new Color(191,191,48), new Color(0,0,0)),
-    GREEN(new Color(0,217,0), new Color(48,191,48), new Color(0,0,0)),
-    BLUE(new Color(0,36,217), new Color(48,72,191), new Color(255,255,255)),
-    INDIGO(new Color(126,0,217), new Color(131,48,191), new Color(255,255,255)),
-    VIOLET(new Color(217,0,217), new Color(191,48,191), new Color(255,255,255)),
+    INFRARED(new Color(0,0,0), new Color(64, 64, 64), new Color(255, 255, 255)),
+    RED(new Color(217,0,0), new Color(255,128,128), new Color(255,255,255)),
+    ORANGE(new Color(217,108,0), new Color(255,191,128), new Color(0,0,0)),
+    YELLOW(new Color(217,217,0), new Color(255,255,128), new Color(0,0,0)),
+    GREEN(new Color(0,217,0), new Color(128,255,128), new Color(0,0,0)),
+    BLUE(new Color(0,36,217), new Color(128,151,255), new Color(255,255,255)),
+    INDIGO(new Color(126,0,217), new Color(200,128,255), new Color(255,255,255)),
+    VIOLET(new Color(217,0,217), new Color(255,128,255), new Color(255,255,255)),
     ULTRAVIOLET(new Color(255,255,255), new Color(191,191,191), new Color(0,0,0));
 
     private Color color;
