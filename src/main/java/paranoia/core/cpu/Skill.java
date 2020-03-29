b486051fb050d0e49d4f5900b3cad778e305b326
Mihalovits Márk <mark.mihalovits@incquerylabs.com>
2020-03-29 21:13:24 +0200

    Added default skills and stats

--- src/main/java/paranoia/core/cpu/Skill.java
+++ src/main/java/paranoia/core/cpu/Skill.java
@@ -1,6 +1,31 @@
 package paranoia.core.cpu;
 
+import static paranoia.core.cpu.Stat.BRAINS;
+import static paranoia.core.cpu.Stat.CHUTZPAH;
+import static paranoia.core.cpu.Stat.MECHANICS;
+import static paranoia.core.cpu.Stat.VIOLENCE;
+
 public class Skill extends ParanoiaModifier{
+
+    public static final Skill ATHLETICS = new Skill("Athletics", VIOLENCE);
+    public static final Skill GUNS = new Skill("Guns", VIOLENCE);
+    public static final Skill MELEE = new Skill("Melee", VIOLENCE);
+    public static final Skill THROW = new Skill("Throw", VIOLENCE);
+
+    public static final Skill SCIENCE = new Skill("Science", BRAINS);
+    public static final Skill PSYCHOLOGY = new Skill("Psychology", BRAINS);
+    public static final Skill BUREAUCRACY = new Skill("Bureaucracy", BRAINS);
+    public static final Skill ALPHA_COMPLEX = new Skill("Alpha Complex", BRAINS);
+
+    public static final Skill BLUFF = new Skill("Bluff", CHUTZPAH);
+    public static final Skill CHARM = new Skill("Charm", CHUTZPAH);
+    public static final Skill INTIMIDATE = new Skill("Intimidate", CHUTZPAH);
+    public static final Skill STEALTH = new Skill("Stalth", CHUTZPAH);
+
+    public static final Skill OPERATE = new Skill("Operate", MECHANICS);
+    public static final Skill ENGINEER = new Skill("Engineer", MECHANICS);
+    public static final Skill PROGRAM = new Skill("Program", MECHANICS);
+    public static final Skill DEMOLITIONS = new Skill("Demolitions", MECHANICS);
 
     private Stat parentStat;
 
--- src/main/java/paranoia/core/cpu/Stat.java
+++ src/main/java/paranoia/core/cpu/Stat.java
@@ -2,6 +2,11 @@ package paranoia.core.cpu;
 
 public class Stat extends ParanoiaModifier{
 
+    public static final Stat VIOLENCE = new Stat("Violence");
+    public static final Stat BRAINS = new Stat("Brains");
+    public static final Stat CHUTZPAH = new Stat("Chutzpah");
+    public static final Stat MECHANICS = new Stat("Mechanics");
+
     public Stat(String name) {
         super(name);
     }
