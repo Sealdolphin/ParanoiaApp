ad92b9e6753c85bb5d495f055cf3304d5dd4e3a2
Mihalovits Márk <mark.mihalovits@incquerylabs.com>
2020-03-31 03:43:21 +0200

    Skill panel uses JTable

--- src/main/java/paranoia/visuals/panels/SkillPanel.java
+++ src/main/java/paranoia/visuals/panels/SkillPanel.java
@@ -4,10 +4,15 @@ import paranoia.core.cpu.ParanoiaAttribute;
 import paranoia.core.cpu.Skill;
 import paranoia.core.cpu.Stat;
 
-import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.GridLayout;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.util.Arrays;
 import java.util.Map;
 
 public class SkillPanel extends JPanel {
@@ -15,27 +20,74 @@ public class SkillPanel extends JPanel {
     public SkillPanel(
         Map<String, ParanoiaAttribute> attributeMap
     ) {
+        //Set UI
+        JTable attributeTable = new JTable(new SkillModel(attributeMap));
+        attributeTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        attributeTable.setDefaultRenderer(Integer.class, new SkillValueRenderer());
 
-        JLabel lbStats = new JLabel("Stats");
-        JPanel statPanel = new JPanel(new GridLayout(1,4));
-        for (Stat stat : Stat.values()) {
-            statPanel.add(new JLabel(stat.toString()));
-            statPanel.add(new JLabel(attributeMap.get(stat.toString()).toString()));
-        }
+        //TODO: refarctor to BoxLayout
+        setLayout(new BorderLayout());
 
-        JLabel lbSkills = new JLabel("Skills");
-        JPanel skillPanel = new JPanel(new GridLayout(4,4));
-        for (Skill skill : Skill.values()) {
-            skillPanel.add(new JLabel(skill.toString()));
-            skillPanel.add(new JLabel(attributeMap.get(skill.toString()).toString()));
-        }
-
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        add(lbStats);
-        add(statPanel);
-        add(lbSkills);
-        add(skillPanel);
-
+        add(attributeTable, BorderLayout.CENTER);
     }
 
+    //TODO: need SkillModel and StatModel separately! -> 2 tables + 2 labels
+    private static class SkillModel extends AbstractTableModel {
+
+        private Map<String, ParanoiaAttribute> attributeMap;
+
+        SkillModel(Map<String, ParanoiaAttribute> attributeMap) {
+            this.attributeMap = attributeMap;
+        }
+
+        @Override
+        public int getRowCount() {
+            return 5;
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 8;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            String key;
+            int ordinal = Math.floorDiv(columnIndex , 2);
+            Stat stat = Stat.values()[ordinal];
+
+            if(rowIndex == 0)
+                key = stat.toString();
+            else
+                key = Arrays.stream(Skill.values())
+                    .filter(skill -> skill.getParent().equals(stat))
+                    .toArray()[rowIndex - 1].toString();
+
+            Integer value = attributeMap.get(key).getValue();
+
+            if(columnIndex % 2 == 0) return key;
+            else return value;
+        }
+
+        @Override
+        public Class<?> getColumnClass(int columnIndex) {
+            if( columnIndex % 2 == 0) return String.class;
+            else return Integer.class;
+        }
+    }
+
+    private static class SkillValueRenderer extends JLabel implements TableCellRenderer {
+
+        @Override
+        public Component getTableCellRendererComponent(
+            JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column
+        ) {
+            //TODO: show colored values (negative = red, positive = green)
+            setText(value.toString());
+            setHorizontalAlignment(CENTER);
+            return this;
+        }
+
+
+    }
 }
