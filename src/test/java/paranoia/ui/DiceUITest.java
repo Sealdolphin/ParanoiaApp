3f2710bd2bce76fb0753a7fe94a5de146f3b7615
Mihalovits Márk <mark.mihalovits@incquerylabs.com>
2020-04-25 18:35:23 +0200

    Basic dialog tests

--- src/test/java/paranoia/ui/DiceUITest.java
+++ src/test/java/paranoia/ui/DiceUITest.java
@@ -3,35 +3,39 @@ package paranoia.ui;
 import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.DialogFixture;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.fixture.JButtonFixture;
+import org.assertj.swing.fixture.JComboBoxFixture;
+import org.assertj.swing.fixture.JLabelFixture;
+import org.assertj.swing.fixture.JPanelFixture;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import paranoia.core.Clone;
+import paranoia.core.Computer;
 import paranoia.core.SecurityClearance;
+import paranoia.core.cpu.ParanoiaAttribute;
 import paranoia.core.cpu.Skill;
 import paranoia.core.cpu.Stat;
+import paranoia.helper.TestClone;
 import paranoia.services.hpdmc.ControlUnit;
+import paranoia.visuals.ComponentName;
 import paranoia.visuals.messages.RollMessage;
 
 import javax.swing.JDialog;
 import java.util.Collections;
-import java.util.Random;
-
-import static paranoia.helper.ParanoiaUtils.testCloneName;
-import static paranoia.helper.ParanoiaUtils.testCloneSector;
-import static paranoia.helper.ParanoiaUtils.testGender;
 
 public class DiceUITest extends AssertJSwingJUnitTestCase {
 
-    private Clone testClone;
+    private TestClone testClone;
     private ControlUnit cpu;
-    private Stat testStat;
-    private Skill testSkill;
     private DialogFixture dialog;
 
-    private final SecurityClearance clearance =
-        SecurityClearance.values()[new Random()
-            .nextInt(SecurityClearance.values().length)];
+
+    private final Stat testStat = Computer.randomItem(Stat.values());
+    private final Skill testSkill = Computer.randomItem(Skill.values());
+    private final SecurityClearance clearance = Computer.randomItem(SecurityClearance.values());
+
     private static final String testMessage = "Please roll with...";
 
     @BeforeClass
@@ -43,8 +47,11 @@ public class DiceUITest extends AssertJSwingJUnitTestCase {
     protected void onSetUp() {
         JDialog rollDialog;
         GuiActionRunner.execute(() -> {
-            testClone = new Clone(testCloneName, testCloneSector, clearance, testGender, 0, null);
+            testClone = new TestClone();
             cpu = new ControlUnit(testClone);
+            testClone.attributes.iterator().forEachRemaining(paranoiaAttribute ->
+                cpu.updateAsset(paranoiaAttribute, ComponentName.SKILL_PANEL));
+
         });
         rollDialog = GuiActionRunner.execute(() -> new RollMessage(
             cpu, testStat, false, testSkill, false,
@@ -56,14 +63,81 @@ public class DiceUITest extends AssertJSwingJUnitTestCase {
     }
 
     @Test
-    public void rollMessageTest() {
+    public void basicMessageTest() {
         //Summon Roll Message dialog
         dialog.requireModal();
+        Integer testSkillValue = getTestSkill();
+        Integer testStatValue = getTestStat();
+
+        //Find test properties
+        JComboBoxFixture skill = dialog.comboBox("cbSkill");
+        JComboBoxFixture stat = dialog.comboBox("cbStat");
+        JLabelFixture lbSkill = dialog.label("lbSkill");
+        JLabelFixture lbStat = dialog.label("lbStat");
+        JLabelFixture lbNode = dialog.label("lbNode");
+
         //test combo boxes
-
+        skill.requireSelection(testSkill.toString());
+        stat.requireSelection(testStat.toString());
         //test labels
-
+        lbSkill.requireText(testSkillValue.toString());
+        lbStat.requireText(testStatValue.toString());
         //test positive + negative
 
+        //Calculate node
+        int node = testSkillValue + testStatValue;
+        lbNode.requireText(Integer.toString(node));
+
+    }
+
+    @Test
+    public void exitMessageTest() {
+        //Close dialog
+        dialog.close();
+        //Assert roll action to false
+        //Assert roll action value
+        FrameFixture coreTech = new FrameFixture(robot(), cpu.getVisuals());
+        coreTech.show();
+        JPanelFixture miscPanel = coreTech.panel(ComponentName.MISC_PANEL.name());
+        Assert.assertEquals(0, miscPanel.target().getComponentCount());
+    }
+
+    @Test
+    public void rollMessageTest() {
+        //Click roll button
+        dialog.requireVisible();
+        JButtonFixture btnRoll = dialog.button("btnRoll");
+        btnRoll.click();
+        //Assert dialog closed
+        dialog.requireNotVisible();
+
+        //Assert roll action value
+        FrameFixture coreTech = new FrameFixture(robot(), cpu.getVisuals());
+        coreTech.show();
+        JPanelFixture miscPanel = coreTech.panel(ComponentName.MISC_PANEL.name());
+        JPanelFixture dicePanel = miscPanel.panel(ComponentName.DICE_PANEL.name());
+        dicePanel.requireEnabled();
+        dicePanel.requireVisible();
+        int dice = Math.abs(getTestSkill() + getTestStat()) + 1;
+        Assert.assertEquals(dice, dicePanel.target().getComponentCount());
+        //Close roll panel
+        miscPanel.button().click();
+        Assert.assertEquals(0, miscPanel.target().getComponentCount());
+    }
+
+    private int getTestSkill() {
+        return testClone.attributes.stream()
+            .filter(attr -> attr.getName().equals(testSkill.toString()))
+            //If not found it is 0 -> not initialized
+            .findFirst().orElse(new ParanoiaAttribute(testSkill.toString(), 0))
+            .getValue();
+    }
+
+    private int getTestStat() {
+        return testClone.attributes.stream()
+            .filter(attr -> attr.getName().equals(testStat.toString()))
+            //If not found it is 0 -> not initialized
+            .findFirst().orElse(new ParanoiaAttribute(testStat.toString(), 0))
+            .getValue();
     }
 }
